250708c43a53239672d6f62393d5d5f0134de940
Nuengnakhap <nueng1569@hotmail.com>
2022-03-29 21:02:11 +0700

    fix bug

--- ios/UnityPlayTsViewManager.m
+++ ios/UnityPlayTsViewManager.m
@@ -11,10 +11,6 @@
 RCT_EXPORT_MODULE(UnityPlayTsView)
 RCT_EXPORT_VIEW_PROPERTY(onUnityMessage, RCTBubblingEventBlock)
 
-- (NSArray<NSString *> *)supportedEvents {
-    return @[@"onUnityMessage"];
-}
-
 - (UIView *)view
 {
     UnityPlayTsView *unity = [UnityPlayTsView new];
